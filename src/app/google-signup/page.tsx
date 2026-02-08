a0096b148758d044307cadf96456872679919abf
kimminseo6 <kimminseoab@gmail.com>
2026-02-08 23:30:57 +0900

    구글로그인 주석

--- src/app/google-signup/page.tsx
+++ src/app/google-signup/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+/**"use client";
 
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -161,4 +161,4 @@ export default function GoogleSignupPage() {
       />
     </div>
   );
-}
+} **/
